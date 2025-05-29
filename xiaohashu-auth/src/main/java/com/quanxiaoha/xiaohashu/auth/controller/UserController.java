8db24e516153eb5cc8ba010f9ee6b1e2bef3b0a7
Bowen Lei <33174642+tengta119@users.noreply.github.com>
2025-05-29 15:14:53 +0800

    初始化

---
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.quanxiaoha.xiaohashu.auth.controller;
+
+import com.quanxiaoha.framework.biz.operationlog.aspect.ApiOperationLog;
+import com.quanxiaoha.framework.common.response.Response;
+import com.quanxiaoha.xiaohashu.auth.model.vo.user.UserLoginReqVO;
+import com.quanxiaoha.xiaohashu.auth.service.UserService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @PostMapping("/login")
+    @ApiOperationLog(description = "用户登录/注册")
+    public Response<String> loginAndRegister(@Validated @RequestBody UserLoginReqVO userLoginReqVO) {
+        return userService.loginAndRegister(userLoginReqVO);
+    }
+
+
+
+}
---
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/service/UserService.java
@@ -0,0 +1,21 @@
+package com.quanxiaoha.xiaohashu.auth.service;
+
+import com.quanxiaoha.framework.common.response.Response;
+import com.quanxiaoha.xiaohashu.auth.model.vo.user.UserLoginReqVO;
+
+/**
+ * @author: 犬小哈
+ * @date: 2024/4/7 15:41
+ * @version: v1.0.0
+ * @description: TODO
+ **/
+public interface UserService {
+
+    /**
+     * 登录与注册
+     * @param userLoginReqVO
+     * @return
+     */
+    Response<String> loginAndRegister(UserLoginReqVO userLoginReqVO);
+
+}
