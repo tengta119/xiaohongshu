c13f062acf26042510e770c796559007a570d80c
Bowen Lei <33174642+tengta119@users.noreply.github.com>
2025-06-09 16:36:06 +0800

    feat: 17、引入快手 BufferTrigger，

--- xiaohashu-count/xiaohashu-count-biz/src/main/java/com/quanxiaoha/xiaohashu/count/biz/consumer/CountFansConsumer.java
+++ xiaohashu-count/xiaohashu-count-biz/src/main/java/com/quanxiaoha/xiaohashu/count/biz/consumer/CountFansConsumer.java
@@ -1,11 +1,16 @@
 package com.quanxiaoha.xiaohashu.count.biz.consumer;
 
 
+import com.github.phantomthief.collection.BufferTrigger;
+import com.quanxiaoha.framework.common.util.JsonUtils;
 import com.quanxiaoha.xiaohashu.count.biz.constant.MQConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.List;
 
 /**
  * @author lbwxxc
@@ -17,13 +22,22 @@ import org.springframework.stereotype.Component;
         topic = MQConstants.TOPIC_COUNT_FANS // 主题 Topic
 )
 @Slf4j
-public class CountFansConsumer implements RocketMQListener<String> { //
+public class CountFansConsumer implements RocketMQListener<String> {
 
+    private BufferTrigger<String> bufferTrigger = BufferTrigger.<String>batchBlocking()
+            .bufferSize(50000)
+            .batchSize(1000)
+            .linger(Duration.ofSeconds(1))
+            .setConsumerEx(this::consumeMessage)
+            .build();
 
     @Override
     public void onMessage(String body) {
-        log.info("## 消费到了 MQ 【计数: 粉丝数】, {}...", body);
+        bufferTrigger.enqueue(body);
     }
 
-
+    private void consumeMessage(List<String> bodys) {
+        log.info("==> 聚合消息, size: {}", bodys.size());
+        log.info("==> 聚合消息, {}", JsonUtils.toJsonString(bodys));
+    }
 }
